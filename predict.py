ceaee4eb21b4bf1d192d0af3152db1546970d3a5
bremer21031986 <80687264+bremer21031986@users.noreply.github.com>
2021-04-02 14:43:48 +0200

    Update predict.py

--- predict.py
+++ predict.py
@@ -21,7 +21,7 @@ parser = argparse.ArgumentParser(description = 'Parser for predict.py')
 
 parser.add_argument('input', default='./flowers/test/56/image_02779.jpg', nargs='?', action='store', type=str)
 parser.add_argument('--dir', action="store",dest="data_dir", default="./flowers/")
-parser.add_argument('checkpoint_ICP', default='./checkpoint_ICP.pth', nargs='?', action="store", type = str)
+parser.add_argument('checkpoint', default='./checkpoint.pth', nargs='?', action="store", type = str)
 parser.add_argument('--top_k', default=5, dest="top_k", action="store", type=int)
 parser.add_argument('--category_names', dest="category_names", action="store", default='cat_to_name.json')
 parser.add_argument('--gpu', default="gpu", action="store", dest="gpu")
@@ -31,7 +31,7 @@ path_image = args.input
 number_of_outputs = args.top_k
 device = args.gpu
 
-path = args.checkpoint_ICP
+path = args.checkpoint
 
 def main():
     model = fmodel.load_checkpoint(path)
@@ -39,7 +39,7 @@ def main():
         cat_to_name = json.load(json_file)
         
     pbs = fmodel.predict(path_image, model, number_of_outputs, device)
-    labels = [cat_to_name[str(index + 1) for index in np.array(pbs[1][0])]]
+    labels = [cat_to_name[str(index + 1)] for index in np.array(pbs[1][0])]
     pb = np.array(pbs[0][0])
     i = 0
     while i < number_of_outputs:
